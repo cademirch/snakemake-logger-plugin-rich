13b1f6525ea34e9c06e005bddb655c5af9b18248
Pavel V. Dimens <19176506+pdimens@users.noreply.github.com>
2025-07-31 15:37:36 -0400

    updates and refactor (#12)
    * adjust snakefile to have progress show a lil better
    
    * initial revisions
    
    * match up keys better
    
    * cleanup
    
    * modify test set, rm some deperacated calls, try hierarchical output
    
    * feat: Refactor eventhandler (#13)
    
    * refactor: move ui managment to console class
    
    * refactor: event_handler to manage log routing and main logic
    
    * refactor: use dataclasses instead of pydantic
    
    * refactor: rewire loghandler to use event handler and console
    
    * remove console wrapper, add progress updater, simplify
    
    * deps: add conda to dev deps
    
    * add conda spinner
    
    * remove log path, close event handler
    
    * make panel'd live display, cleanup conda statuses
    
    * Shell: => Shell Command: and indented code block beneath
    
    * add finish text with logfile
    
    * enable shellcmd wordwrapping
    
    * pasimonious submit/finished text
    
    * turn over-engineering into a simpler and nicer solution
    
    * dim green when finished
    
    * add DAG status spinner
    
    * add failure
    
    * force showing log files for now
    
    * live panels
    
    * dynamic panel heights and printshellcmd support
    
    * update
    
    ---------
    
    Co-authored-by: Cade Mirchandani <cmirchan@ucsc.edu>

--- src/snakemake_logger_plugin_rich/handler.py
+++ src/snakemake_logger_plugin_rich/handler.py
@@ -1,8 +1,12 @@
 import logging
 from rich.logging import RichHandler
-from snakemake_interface_logger_plugins.settings import OutputSettingsLoggerInterface
-from rich.console import Console
 from rich.progress import Progress, BarColumn, TextColumn, TimeElapsedColumn
+from rich.console import Console
+from rich.layout import Layout
+from rich.live import Live
+from rich import box
+from rich.panel import Panel
+from snakemake_interface_logger_plugins.settings import OutputSettingsLoggerInterface
 from snakemake_logger_plugin_rich.event_handler import EventHandler
 
 
@@ -19,27 +23,50 @@ class RichLogHandler(RichHandler):
         **kwargs,
     ):
         self.settings = settings
-        self.console = Console()
+        self.console = Console(log_path=False, stderr=True)
         self.progress = Progress(
             TextColumn("[bold blue]{task.description}"),
-            BarColumn(complete_style="green"),
+            BarColumn(bar_width=None, complete_style="green", finished_style= "dim green"),
             TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
             TimeElapsedColumn(),
             console=self.console,
-            transient=False,
+            transient=True,
+            expand=True,
             auto_refresh=False,
+            disable=True,
         )
-
-        if not self.settings.dryrun:
-            self.progress.start()
+        self.layout = Layout()
+        self.layout.split_column(
+            Layout(Panel("◯ Last Submitted", box = box.SIMPLE, padding = 0), name = "submitted", minimum_size =4, size = 15),
+            Layout(Panel("◉ Last Finished", box = box.SIMPLE, padding = 0), name = "finished", size=3),
+            Layout(
+                Panel(
+                    self.progress,
+                    title = "Workflow Progress",
+                    border_style="dim",
+                    padding = (0,1,0,1)
+                ),
+                name = "progress", size=1, minimum_size=1
+            )
+        )
+        self.live_display = Live(
+            self.layout,
+            refresh_per_second=8,
+            transient=True,
+            console= self.console
+        )
 
         self.event_handler = EventHandler(
             console=self.console,
             progress=self.progress,
+            layout=self.layout,
+            live_display=self.live_display,
             dryrun=self.settings.dryrun,
+            printshellcmds=self.settings.printshellcmds,
+            show_failed_logs=settings.show_failed_logs
         )
+
         kwargs["console"] = self.console
-        kwargs["show_path"] = True
         kwargs["show_time"] = True
         kwargs["omit_repeated_times"] = False
         kwargs["rich_tracebacks"] = True
@@ -67,4 +94,6 @@ class RichLogHandler(RichHandler):
 
     def close(self):
         """Clean up resources."""
+        self.event_handler.close()
+        self.console.clear_live()
         super().close()
